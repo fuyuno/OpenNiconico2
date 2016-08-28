aee4a12c2fe88ebc8e6aef291b2d3d4a07222453
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-08-29 04:28:35 +0900

    ニコる廃止による修正

--- Mntone.Nico2/Mntone.Nico2.Shared/Users/Info/InfoResponse.cs
+++ Mntone.Nico2/Mntone.Nico2.Shared/Users/Info/InfoResponse.cs
@@ -1,122 +1,123 @@
-﻿using HtmlAgilityPack;
-using System.Linq;
+﻿using System.Linq;
+
+using HtmlAgilityPack;
 
 namespace Mntone.Nico2.Users.Info
 {
-	/// <summary>
-	/// ユーザー情報を格納するクラス
-	/// </summary>
-	public sealed class InfoResponse
-	{
-		internal InfoResponse( HtmlNode bodyHtml, string language )
-		{
-			var profileHtml = bodyHtml.GetElementByClassName( "userDetail" ).GetElementByClassName( "profile" );
-			{
-				var h2Html = profileHtml.Element( "h2" );
-				this.Name = h2Html.FirstChild.InnerText;
-			}
+    /// <summary>
+    ///     ユーザー情報を格納するクラス
+    /// </summary>
+    public sealed class InfoResponse
+    {
+        /// <summary>
+        ///     名前
+        /// </summary>
+        public string Name { get; private set; }
 
-			{
-				var accountIdHtml = profileHtml.GetElementByClassName( "account" ).GetElementByClassName( "accountNumber" ).Element( "span" );
-				var keywords = accountIdHtml.InnerText.Split( new char[] { ' ', '(', ')' } );
-				if( keywords.Count() >= 4 )
-				{
-					switch( language )
-					{
-					case "ja-jp":
-						this.Id = keywords[0].ToUInt();
-						this.JoinedVersion = keywords[1];
-						this.IsPremium = keywords[3] == "プレミアム会員";
-						break;
-					case "en-us":
-						this.Id = keywords[0].ToUInt();
-						this.JoinedVersion = keywords[1];
-						this.IsPremium = keywords[3] == "Premium";
-						break;
-					case "zh-tw":
-						this.Id = keywords[0].ToUInt();
-						this.JoinedVersion = keywords[1];
-						this.IsPremium = keywords[3] == "白金會員";
-						break;
-					}
-				}
-			}
+        /// <summary>
+        ///     ユーザー ID
+        /// </summary>
+        public uint Id { get; private set; }
 
-			{
-				var statsHtml = profileHtml.GetElementByClassName( "stats" );
-				this.FavoriteCount = statsHtml.GetElementByClassName( "fav" ).FirstChild.InnerText.ToUShort();
+        /// <summary>
+        ///     アカウントが加入したときのバージョン
+        /// </summary>
+        public string JoinedVersion { get; private set; }
 
-				var stampText = statsHtml.GetElementByClassName( "exp" ).FirstChild.InnerText;
-				this.StampCount = stampText.Substring( 0, stampText.Length - 3 ).ToUShort();
+        /// <summary>
+        ///     プレミアムか
+        /// </summary>
+        public bool IsPremium { get; private set; }
 
-				var nicoruText = statsHtml.GetElementByClassName( "nicoru" ).FirstChild.ChildNodes[1].InnerText;
-				if( nicoruText != "-" )
-				{
-					this.NicoruCount = nicoruText.ToUShort();
-				}
+        /// <summary>
+        ///     お気に入り登録された数
+        /// </summary>
+        public ushort FavoriteCount { get; private set; }
 
-				var pointsText = statsHtml.GetElementByClassName( "nicopoint" ).FirstChild.InnerText;
-				this.Points = pointsText.Substring( 0, pointsText.Length - 2 ).ToUInt();
+        /// <summary>
+        ///     スタンプ数
+        /// </summary>
+        public ushort StampCount { get; private set; }
 
-				var creatorScoreText = statsHtml.GetElementByClassName( "cpp" ).FirstChild.InnerText;
-				switch( language )
-				{
-				case "ja-jp":
-				case "zh-tw":
-					this.CreatorScore = creatorScoreText.Substring( 0, creatorScoreText.Length - 1 ).ToUInt();
-					break;
-				case "en-us":
-					this.CreatorScore = creatorScoreText.Substring( 0, creatorScoreText.Length - 7 ).ToUInt();
-					break;
-				default:
-					break;
-				}
-			}
-		}
+        /// <summary>
+        ///     ニコる数
+        /// </summary>
+        public ushort NicoruCount { get; private set; }
 
-		/// <summary>
-		/// 名前
-		/// </summary>
-		public string Name { get; private set; }
+        /// <summary>
+        ///     ニコニコポイント数
+        /// </summary>
+        public uint Points { get; private set; }
 
-		/// <summary>
-		/// ユーザー ID
-		/// </summary>
-		public uint Id { get; private set; }
+        /// <summary>
+        ///     クリエイター推奨スコア
+        /// </summary>
+        public uint CreatorScore { get; private set; }
 
-		/// <summary>
-		/// アカウントが加入したときのバージョン
-		/// </summary>
-		public string JoinedVersion { get; private set; }
+        internal InfoResponse(HtmlNode bodyHtml, string language)
+        {
+            var profileHtml = bodyHtml.GetElementByClassName("userDetail").GetElementByClassName("profile");
+            {
+                var h2Html = profileHtml.Element("h2");
+                Name = h2Html.FirstChild.InnerText;
+            }
 
-		/// <summary>
-		/// プレミアムか
-		/// </summary>
-		public bool IsPremium { get; private set; }
+            {
+                var accountIdHtml = profileHtml.GetElementByClassName("account").GetElementByClassName("accountNumber").Element("span");
+                var keywords = accountIdHtml.InnerText.Split(' ', '(', ')');
+                if (keywords.Count() >= 4)
+                    switch (language)
+                    {
+                        case "ja-jp":
+                            Id = keywords[0].ToUInt();
+                            JoinedVersion = keywords[1];
+                            IsPremium = keywords[3] == "プレミアム会員";
+                            break;
 
-		/// <summary>
-		/// お気に入り登録された数
-		/// </summary>
-		public ushort FavoriteCount { get; private set; }
+                        case "en-us":
+                            Id = keywords[0].ToUInt();
+                            JoinedVersion = keywords[1];
+                            IsPremium = keywords[3] == "Premium";
+                            break;
 
-		/// <summary>
-		/// スタンプ数
-		/// </summary>
-		public ushort StampCount { get; private set; }
+                        case "zh-tw":
+                            Id = keywords[0].ToUInt();
+                            JoinedVersion = keywords[1];
+                            IsPremium = keywords[3] == "白金會員";
+                            break;
+                    }
+            }
 
-		/// <summary>
-		/// ニコる数
-		/// </summary>
-		public ushort NicoruCount { get; private set; }
+            {
+                var statsHtml = profileHtml.GetElementByClassName("stats");
+                FavoriteCount = statsHtml.GetElementByClassName("fav").FirstChild.InnerText.ToUShort();
 
-		/// <summary>
-		/// ニコニコポイント数
-		/// </summary>
-		public uint Points { get; private set; }
+                var stampText = statsHtml.GetElementByClassName("exp").FirstChild.InnerText;
+                StampCount = stampText.Substring(0, stampText.Length - 3).ToUShort();
 
-		/// <summary>
-		/// クリエイター推奨スコア
-		/// </summary>
-		public uint CreatorScore { get; private set; }
-	}
+                var nicoruText = statsHtml.GetElementByClassName("nicoru")?.FirstChild.ChildNodes[1].InnerText ?? "-";
+                if (nicoruText != "-")
+                    NicoruCount = nicoruText.ToUShort();
+
+                var pointsText = statsHtml.GetElementByClassName("nicopoint").FirstChild.InnerText;
+                Points = pointsText.Substring(0, pointsText.Length - 2).ToUInt();
+
+                var creatorScoreText = statsHtml.GetElementByClassName("cpp").FirstChild.InnerText;
+                switch (language)
+                {
+                    case "ja-jp":
+                    case "zh-tw":
+                        CreatorScore = creatorScoreText.Substring(0, creatorScoreText.Length - 1).ToUInt();
+                        break;
+
+                    case "en-us":
+                        CreatorScore = creatorScoreText.Substring(0, creatorScoreText.Length - 7).ToUInt();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
 }
