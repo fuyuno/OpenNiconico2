20f74ec235aaebb6f6aec6cc4de6936052df2926
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-08-29 05:12:01 +0900

    ログアウト URL の修正

--- Mntone.Nico2/Mntone.Nico2.Shared/NiconicoUrls.cs
+++ Mntone.Nico2/Mntone.Nico2.Shared/NiconicoUrls.cs
@@ -1,263 +1,270 @@
-﻿using System;
-
-namespace Mntone.Nico2
+﻿namespace Mntone.Nico2
 {
-	/// <summary>
-	/// ニコニコに関する URL 群
-	/// </summary>
-	public static class NiconicoUrls
-	{
-		private const string DomainBase = ".nicovideo.jp/";
+    /// <summary>
+    ///     ニコニコに関する URL 群
+    /// </summary>
+    public static class NiconicoUrls
+    {
+        private const string DomainBase = ".nicovideo.jp/";
 
-		private const string LiveApiForCommunityUrlBase = "http://watch.live.nicovideo.jp/api/";
-		private const string LiveApiForOfficialOrChannelUrlBase = "http://ow.live.nicovideo.jp/api/";
-		private const string LiveApiForExternalUrlBase = "http://ext.live.nicovideo.jp/api/";
+        private const string LiveApiForCommunityUrlBase = "http://watch.live.nicovideo.jp/api/";
+        private const string LiveApiForOfficialOrChannelUrlBase = "http://ow.live.nicovideo.jp/api/";
+        private const string LiveApiForExternalUrlBase = "http://ext.live.nicovideo.jp/api/";
 
-		/// <summary>
-		/// ニコニコ トップ ページ URL テキスト
-		/// </summary>
-		public static string TopPageUrl { get { return VideoUrlBase; } }
+        /// <summary>
+        ///     ニコニコ トップ ページ URL テキスト
+        /// </summary>
+        public static string TopPageUrl => VideoUrlBase;
 
+        #region Authentication
 
-		#region Authentication
+        private const string AuthenticationBase = "https://secure" + DomainBase + "secure/";
 
-		private const string AuthenticationBase = "https://secure" + DomainBase + "secure/";
+        internal static string LogOnUrl => AuthenticationBase + "login?site=niconico";
+        internal static string LogOffUrl => "https://account.nicovideo.jp/logout?site=niconico";
 
-		internal static string LogOnUrl { get { return AuthenticationBase + "login?site=niconico"; } }
-		internal static string LogOffUrl { get { return AuthenticationBase + "logout"; } }
+        #endregion
 
-		#endregion
-
-
-		#region Videos
+        #region Videos
 
-		private const string VideoUrlBase = "http://www" + DomainBase;
-		private const string VideoApiUrlBase = VideoUrlBase + "api/";
-		private const string VideoFlapiUrlBase = "http://flapi" + DomainBase + "api/";
-
-		/// <summary>
-		/// ニコニコ動画 トップ ページ URL テキスト
-		/// </summary>
-		public static string VideoTopPageUrl { get { return VideoUrlBase + "my/top"; } }
-
-		/// <summary>
-		/// ニコニコ動画 マイ ページ URL テキスト
-		/// </summary>
-		public static string VideoMyPageUrl { get { return VideoUrlBase + "video_top"; } }
-
-		/// <summary>
-		/// ニコニコ動画 視聴ページ URL テキスト
-		/// </summary>
-		public static string VideoWatchPageUrl { get { return VideoUrlBase + "watch/"; } }
+        private const string VideoUrlBase = "http://www" + DomainBase;
+        private const string VideoApiUrlBase = VideoUrlBase + "api/";
+        private const string VideoFlapiUrlBase = "http://flapi" + DomainBase + "api/";
 
-		internal static string VideoFlvUrl { get { return VideoFlapiUrlBase + "getflv/"; } }
-		internal static string VideoThumbInfoUrl { get { return "http://ext.nicovideo.jp/api/getthumbinfo/"; } }
-		internal static string VideoHistoryUrl { get { return VideoApiUrlBase + "videoviewhistory/list"; } }
-		internal static string VideoRemoveUrl { get { return VideoApiUrlBase + "videoviewhistory/remove?token="; } }
+        /// <summary>
+        ///     ニコニコ動画 トップ ページ URL テキスト
+        /// </summary>
+        public static string VideoTopPageUrl => VideoUrlBase + "my/top";
 
-		#endregion
+        /// <summary>
+        ///     ニコニコ動画 マイ ページ URL テキスト
+        /// </summary>
+        public static string VideoMyPageUrl => VideoUrlBase + "video_top";
 
+        /// <summary>
+        ///     ニコニコ動画 視聴ページ URL テキスト
+        /// </summary>
+        public static string VideoWatchPageUrl => VideoUrlBase + "watch/";
 
-		#region Live
+        internal static string VideoFlvUrl => VideoFlapiUrlBase + "getflv/";
 
-		private const string LiveUrlBase = "http://live" + DomainBase;
-		private const string LiveApiUrlBase = LiveUrlBase + "api/";
+        internal static string VideoThumbInfoUrl => "http://ext.nicovideo.jp/api/getthumbinfo/";
 
-		/// <summary>
-		/// ニコニコ生放送 トップ ページ URL テキスト
-		/// </summary>
-		public static string LiveTopPageUrl { get { return LiveUrlBase; } }
+        internal static string VideoHistoryUrl => VideoApiUrlBase + "videoviewhistory/list";
 
-		/// <summary>
-		/// ニコニコ生放送 マイ ページ URL テキスト
-		/// </summary>
-		public static string LiveMyPageUrl { get { return LiveUrlBase + "my"; } }
+        internal static string VideoRemoveUrl => VideoApiUrlBase + "videoviewhistory/remove?token=";
 
-		/// <summary>
-		/// ニコニコ生放送 視聴ページ URL テキスト
-		/// </summary>
-		public static string LiveWatchPageUrl { get { return LiveUrlBase + "watch/"; } }
+        #endregion
 
-		/// <summary>
-		/// ニコニコ生放送 ゲート ページ URL テキスト
-		/// </summary>
-		public static string LiveGatePageUrl { get { return LiveUrlBase + "gate/"; } }
+        #region Live
 
-		internal static string LiveCKeyUrl { get { return LiveApiUrlBase + "getckey"; } }
-		internal static string LivePlayerStatusUrl { get { return LiveApiUrlBase + "getplayerstatus/"; } }
-		internal static string LivePostKeyUrl { get { return LiveApiUrlBase + "getpostkey"; } }
-		internal static string LiveVoteUrl { get { return LiveApiUrlBase + "vote"; } }
-		internal static string LiveHeartbeatUrl { get { return LiveApiUrlBase + "heartbeat"; } }
-		internal static string LiveLeaveUrl { get { return LiveApiUrlBase + "leave"; } }
-		internal static string LiveTagRevisionUrl { get { return LiveApiUrlBase + "tagrev/"; } }
-		internal static string LiveZappingListIndexUrl { get { return LiveApiUrlBase + "getzappinglist?zroute=index"; } }
-		internal static string LiveZappingListRecentUrl { get { return LiveApiUrlBase + "getzappinglist?zroute=recent"; } }
-		internal static string LiveIndexZeroStreamListUrl { get { return LiveApiUrlBase + "getindexzerostreamlist?status="; } }
-		internal static string LiveWatchingReservationListUrl { get { return LiveApiUrlBase + "watchingreservation?mode=list"; } }
-		internal static string LiveWatchingReservationDetailListUrl { get { return LiveApiUrlBase + "watchingreservation?mode=detaillist"; } }
+        private const string LiveUrlBase = "http://live" + DomainBase;
+        private const string LiveApiUrlBase = LiveUrlBase + "api/";
 
-		#endregion
+        /// <summary>
+        ///     ニコニコ生放送 トップ ページ URL テキスト
+        /// </summary>
+        public static string LiveTopPageUrl => LiveUrlBase;
 
+        /// <summary>
+        ///     ニコニコ生放送 マイ ページ URL テキスト
+        /// </summary>
+        public static string LiveMyPageUrl => LiveUrlBase + "my";
 
-		#region Images
+        /// <summary>
+        ///     ニコニコ生放送 視聴ページ URL テキスト
+        /// </summary>
+        public static string LiveWatchPageUrl => LiveUrlBase + "watch/";
 
-		private const string ImageUrlBase = "http://seiga" + DomainBase;
-		private const string ImageApiUrlBase = ImageUrlBase + "api/";
-		private const string ImageExtApiUrlBase = "http://ext.seiga" + DomainBase + "api/";
+        /// <summary>
+        ///     ニコニコ生放送 ゲート ページ URL テキスト
+        /// </summary>
+        public static string LiveGatePageUrl => LiveUrlBase + "gate/";
 
-		/// <summary>
-		/// ニコニコ静画 トップ ページ URL テキスト
-		/// </summary>
-		public static string ImageTopPageUrl { get { return ImageUrlBase; } }
+        internal static string LiveCKeyUrl => LiveApiUrlBase + "getckey";
 
-		/// <summary>
-		/// ニコニコ静画 マイ ページ URL テキスト
-		/// </summary>
-		public static string ImageMyPageUrl { get { return ImageUrlBase + "my"; } }
-		
-		/// <summary>
-		/// ニコニコ静画 お題 トップ ページ URL テキスト
-		/// </summary>
-		public static string ImageThemeTopPageUrl { get { return ImageUrlBase + "theme/"; } }
+        internal static string LivePlayerStatusUrl => LiveApiUrlBase + "getplayerstatus/";
 
-		/// <summary>
-		/// ニコニコ静画 イラスト トップ ページ URL テキスト
-		/// </summary>
-		public static string ImageIllustTopPageUrl { get { return ImageUrlBase + "illust/"; } }
+        internal static string LivePostKeyUrl => LiveApiUrlBase + "getpostkey";
 
-		/// <summary>
-		/// ニコニコ春画 トップ ページ URL テキスト
-		/// </summary>
-		public static string ImageIllustAdultTopPageUrl { get { return ImageUrlBase + "shunga/"; } }
+        internal static string LiveVoteUrl => LiveApiUrlBase + "vote";
 
-		/// <summary>
-		/// ニコニコ静画 漫画 トップ ページ URL テキスト
-		/// </summary>
-		public static string ImageMangaTopPageUrl { get { return ImageUrlBase + "manga/"; } }
+        internal static string LiveHeartbeatUrl => LiveApiUrlBase + "heartbeat";
 
-		/// <summary>
-		/// ニコニコ静画 電子書籍 トップ ページ URL テキスト
-		/// </summary>
-		public static string ImageElectronicBookTopPageUrl { get { return ImageUrlBase + "book/"; } }
+        internal static string LiveLeaveUrl => LiveApiUrlBase + "leave";
 
-		internal static string ImageBlogPartsUrl { get { return ImageExtApiUrlBase + "illust/blogparts?mode="; } }
-		internal static string ImageUserInfoUrl { get { return ImageApiUrlBase + "user/info?id="; } }
-		internal static string ImageUserDataUrl { get { return ImageApiUrlBase + "user/data?id="; } }
+        internal static string LiveTagRevisionUrl => LiveApiUrlBase + "tagrev/";
 
-		#endregion
+        internal static string LiveZappingListIndexUrl => LiveApiUrlBase + "getzappinglist?zroute=index";
 
+        internal static string LiveZappingListRecentUrl => LiveApiUrlBase + "getzappinglist?zroute=recent";
 
-		#region Searches
+        internal static string LiveIndexZeroStreamListUrl => LiveApiUrlBase + "getindexzerostreamlist?status=";
 
-		private const string SearchApiUrlBase = "http://api.search" + DomainBase + "api/";
+        internal static string LiveWatchingReservationListUrl => LiveApiUrlBase + "watchingreservation?mode=list";
 
-		internal static string SearchSuggestionUrl { get { return "http://search" + DomainBase + "suggestion/complete/"; } }
+        internal static string LiveWatchingReservationDetailListUrl => LiveApiUrlBase + "watchingreservation?mode=detaillist";
 
-		#endregion
+        #endregion
 
+        #region Images
 
-		#region Dictionaries
+        private const string ImageUrlBase = "http://seiga" + DomainBase;
+        private const string ImageApiUrlBase = ImageUrlBase + "api/";
+        private const string ImageExtApiUrlBase = "http://ext.seiga" + DomainBase + "api/";
 
-		private const string DictionaryUrlBase = "http://dic" + DomainBase;
-		private const string DictionaryApiUrlBase = "http://api.nicodic.jp/";
+        /// <summary>
+        ///     ニコニコ静画 トップ ページ URL テキスト
+        /// </summary>
+        public static string ImageTopPageUrl => ImageUrlBase;
 
-		/// <summary>
-		/// ニコニコ大百科 トップ ページ URL テキスト
-		/// </summary>
-		public static string DictionaryTopPageUrl { get { return DictionaryUrlBase; } }
+        /// <summary>
+        ///     ニコニコ静画 マイ ページ URL テキスト
+        /// </summary>
+        public static string ImageMyPageUrl => ImageUrlBase + "my";
 
-		internal static string DictionaryWordExistUrl { get { return DictionaryApiUrlBase + "e/json/"; } }
-		internal static string DictionarySummarytUrl { get { return DictionaryApiUrlBase + "page.summary/json/a/"; } }
-		internal static string DictionaryExistUrl { get { return DictionaryApiUrlBase + "page.exist/json/"; } }
-		internal static string DictionaryRecentUrl { get { return DictionaryApiUrlBase + "page.created/json"; } }
+        /// <summary>
+        ///     ニコニコ静画 お題 トップ ページ URL テキスト
+        /// </summary>
+        public static string ImageThemeTopPageUrl => ImageUrlBase + "theme/";
 
-		#endregion
+        /// <summary>
+        ///     ニコニコ静画 イラスト トップ ページ URL テキスト
+        /// </summary>
+        public static string ImageIllustTopPageUrl => ImageUrlBase + "illust/";
 
+        /// <summary>
+        ///     ニコニコ春画 トップ ページ URL テキスト
+        /// </summary>
+        public static string ImageIllustAdultTopPageUrl => ImageUrlBase + "shunga/";
 
-		#region Apps
+        /// <summary>
+        ///     ニコニコ静画 漫画 トップ ページ URL テキスト
+        /// </summary>
+        public static string ImageMangaTopPageUrl => ImageUrlBase + "manga/";
 
-		private const string AppUrlBase = "http://app" + DomainBase;
+        /// <summary>
+        ///     ニコニコ静画 電子書籍 トップ ページ URL テキスト
+        /// </summary>
+        public static string ImageElectronicBookTopPageUrl => ImageUrlBase + "book/";
 
-		/// <summary>
-		/// ニコニコアプリ トップ ページ URL テキスト
-		/// </summary>
-		public static string AppTopPageUrl { get { return AppUrlBase; } }
+        internal static string ImageBlogPartsUrl => ImageExtApiUrlBase + "illust/blogparts?mode=";
 
-		/// <summary>
-		/// ニコニコアプリ マイ ページ URL テキスト
-		/// </summary>
-		public static string AppMyPageUrl { get { return AppUrlBase + "my/apps"; } }
+        internal static string ImageUserInfoUrl => ImageApiUrlBase + "user/info?id=";
 
-		#endregion
+        internal static string ImageUserDataUrl => ImageApiUrlBase + "user/data?id=";
 
+        #endregion
 
-		#region Communities
+        #region Searches
 
-		/// <summary>
-		/// ニコニコ コミュニティー アイコン URL テキスト
-		/// {0}: CommunityID / 10000
-		/// {1}: CommunityID
-		/// </summary>
-		public static string CommunityIconUrl { get { return "http://icon.nimg.jp/community/{0}/co{1}.jpg"; } }
+        private const string SearchApiUrlBase = "http://api.search" + DomainBase + "api/";
 
-		/// <summary>
-		/// ニコニコ コミュニティー 小アイコン URL テキスト
-		/// {0}: CommunityID / 10000
-		/// {1}: CommunityID
-		/// </summary>
-		public static string CommunitySmallIconUrl { get { return "http://icon.nimg.jp/community/s/{0}/co{1}.jpg"; } }
+        internal static string SearchSuggestionUrl => "http://search" + DomainBase + "suggestion/complete/";
 
-		/// <summary>
-		/// ニコニコ コミュニティー アイコン未設定 URL テキスト
-		/// </summary>
-		public static string CommunityBlankIconUrl { get { return "http://icon.nimg.jp/404.jpg"; } }
-		
-		#endregion
+        #endregion
 
+        #region Dictionaries
 
-		#region Channels
+        private const string DictionaryUrlBase = "http://dic" + DomainBase;
+        private const string DictionaryApiUrlBase = "http://api.nicodic.jp/";
 
-		/// <summary>
-		/// ニコニコ チャンネル アイコン URL テキスト
-		/// {0}: ChannelID
-		/// </summary>
-		public static string ChannelIconUrl { get { return "http://icon.nimg.jp/channel/ch{0}.jpg"; } }
+        /// <summary>
+        ///     ニコニコ大百科 トップ ページ URL テキスト
+        /// </summary>
+        public static string DictionaryTopPageUrl => DictionaryUrlBase;
 
-		/// <summary>
-		/// ニコニコ チャンネル 小アイコン URL テキスト
-		/// {0}: ChannelID
-		/// </summary>
-		public static string ChannelSmallIconUrl { get { return "http://icon.nimg.jp/channel/s/ch{0}.jpg"; } }
+        internal static string DictionaryWordExistUrl => DictionaryApiUrlBase + "e/json/";
 
-		#endregion
+        internal static string DictionarySummarytUrl => DictionaryApiUrlBase + "page.summary/json/a/";
 
+        internal static string DictionaryExistUrl => DictionaryApiUrlBase + "page.exist/json/";
 
-		#region Users
+        internal static string DictionaryRecentUrl => DictionaryApiUrlBase + "page.created/json";
 
-		/// <summary>
-		/// ニコニコ ユーザー ページ URL テキスト
-		/// </summary>
-		public static string UserPageUrl { get { return VideoUrlBase + "my"; } }
+        #endregion
 
-		/// <summary>
-		/// ニコニコ ユーザー アイコン URL テキスト
-		/// {0}: UserID / 10000
-		/// {1}: UserID
-		/// </summary>
-		public static string UserIconUrl { get { return "http://usericon.nimg.jp/usericon/{0}/{1}.jpg"; } }
+        #region Apps
 
-		/// <summary>
-		/// ニコニコ ユーザー 小アイコン URL テキスト
-		/// {0}: UserID / 10000
-		/// {1}: UserID
-		/// </summary>
-		public static string UserSmallIconUrl { get { return "http://usericon.nimg.jp/usericon/s/{0}/{1}.jpg"; } }
+        private const string AppUrlBase = "http://app" + DomainBase;
 
-		/// <summary>
-		/// ニコニコ ユーザー アイコン未設定 URL テキスト
-		/// </summary>
-		public static string UserBlankIconUrl { get { return "http://uni.res.nimg.jp/img/user/thumb/blank.jpg"; } }
-		
-		#endregion
-	}
+        /// <summary>
+        ///     ニコニコアプリ トップ ページ URL テキスト
+        /// </summary>
+        public static string AppTopPageUrl => AppUrlBase;
+
+        /// <summary>
+        ///     ニコニコアプリ マイ ページ URL テキスト
+        /// </summary>
+        public static string AppMyPageUrl => AppUrlBase + "my/apps";
+
+        #endregion
+
+        #region Communities
+
+        /// <summary>
+        ///     ニコニコ コミュニティー アイコン URL テキスト
+        ///     {0}: CommunityID / 10000
+        ///     {1}: CommunityID
+        /// </summary>
+        public static string CommunityIconUrl => "http://icon.nimg.jp/community/{0}/co{1}.jpg";
+
+        /// <summary>
+        ///     ニコニコ コミュニティー 小アイコン URL テキスト
+        ///     {0}: CommunityID / 10000
+        ///     {1}: CommunityID
+        /// </summary>
+        public static string CommunitySmallIconUrl => "http://icon.nimg.jp/community/s/{0}/co{1}.jpg";
+
+        /// <summary>
+        ///     ニコニコ コミュニティー アイコン未設定 URL テキスト
+        /// </summary>
+        public static string CommunityBlankIconUrl => "http://icon.nimg.jp/404.jpg";
+
+        #endregion
+
+        #region Channels
+
+        /// <summary>
+        ///     ニコニコ チャンネル アイコン URL テキスト
+        ///     {0}: ChannelID
+        /// </summary>
+        public static string ChannelIconUrl => "http://icon.nimg.jp/channel/ch{0}.jpg";
+
+        /// <summary>
+        ///     ニコニコ チャンネル 小アイコン URL テキスト
+        ///     {0}: ChannelID
+        /// </summary>
+        public static string ChannelSmallIconUrl => "http://icon.nimg.jp/channel/s/ch{0}.jpg";
+
+        #endregion
+
+        #region Users
+
+        /// <summary>
+        ///     ニコニコ ユーザー ページ URL テキスト
+        /// </summary>
+        public static string UserPageUrl => VideoUrlBase + "my";
+
+        /// <summary>
+        ///     ニコニコ ユーザー アイコン URL テキスト
+        ///     {0}: UserID / 10000
+        ///     {1}: UserID
+        /// </summary>
+        public static string UserIconUrl => "http://usericon.nimg.jp/usericon/{0}/{1}.jpg";
+
+        /// <summary>
+        ///     ニコニコ ユーザー 小アイコン URL テキスト
+        ///     {0}: UserID / 10000
+        ///     {1}: UserID
+        /// </summary>
+        public static string UserSmallIconUrl => "http://usericon.nimg.jp/usericon/s/{0}/{1}.jpg";
+
+        /// <summary>
+        ///     ニコニコ ユーザー アイコン未設定 URL テキスト
+        /// </summary>
+        public static string UserBlankIconUrl => "http://uni.res.nimg.jp/img/user/thumb/blank.jpg";
+
+        #endregion
+    }
 }
