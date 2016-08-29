b64de95f2fe6b6ba29f2e3d9135cb74b4f979140
Mikazuki Fuyuno <mikazuki_fuyuno@outlook.com>
2016-08-30 03:14:04 +0900

    view_counter が null を返してくるのに対応

--- Mntone.Nico2/Mntone.Nico2.Shared/Live/OnAirStreams/OnAirStreamsClient.cs
+++ Mntone.Nico2/Mntone.Nico2.Shared/Live/OnAirStreams/OnAirStreamsClient.cs
@@ -3,55 +3,54 @@ using System.Threading.Tasks;
 
 namespace Mntone.Nico2.Live.OnAirStreams
 {
-	internal sealed class OnAirStreamsClient
-	{
-		public static Task<string> GetOnAirStreamsIndexDataAsync( NiconicoContext context, ushort pageIndex )
-		{
-			var sb = new StringBuilder( NiconicoUrls.LiveZappingListIndexUrl );
-			if( pageIndex != 1 )
-			{
-				sb.Append( "&zpage=" );
-				sb.Append( pageIndex );
-			}
-			return context.GetClient().GetStringAsync( sb.ToString() );
-		}
+    internal sealed class OnAirStreamsClient
+    {
+        public static Task<string> GetOnAirStreamsIndexDataAsync(NiconicoContext context, ushort pageIndex)
+        {
+            var sb = new StringBuilder(NiconicoUrls.LiveZappingListIndexUrl);
+            if (pageIndex != 1)
+            {
+                sb.Append("&zpage=");
+                sb.Append(pageIndex);
+            }
+            return context.GetClient().GetStringAsync(sb.ToString());
+        }
 
-		public static Task<string> GetOnAirStreamsRecentDataAsync(
-			NiconicoContext context, ushort pageIndex, Category category, SortDirection direction, SortType type )
-		{
-			var sb = new StringBuilder( NiconicoUrls.LiveZappingListRecentUrl );
-			if( pageIndex != 1 )
-			{
-				sb.Append( "&zpage=" );
-				sb.Append( pageIndex );
-			}
-			sb.Append( "&tab=" );
-			sb.Append( category.ToCategoryString() );
-			if( direction == SortDirection.Descending )
-			{
-				sb.Append( "&order=desc" );
-			}
-			sb.Append( "&sort=" );
-			sb.Append( type.ToSortTypeString() );
-			return context.GetClient().GetStringAsync( sb.ToString() );
-		}
+        public static Task<string> GetOnAirStreamsRecentDataAsync(
+            NiconicoContext context, ushort pageIndex, Category category, SortDirection direction, SortType type)
+        {
+            var sb = new StringBuilder(NiconicoUrls.LiveZappingListRecentUrl);
+            if (pageIndex != 1)
+            {
+                sb.Append("&zpage=");
+                sb.Append(pageIndex);
+            }
+            sb.Append("&tab=");
+            sb.Append(category.ToCategoryString());
+            if (direction == SortDirection.Descending)
+                sb.Append("&order=desc");
+            sb.Append("&sort=");
+            sb.Append(type.ToSortTypeString());
+            return context.GetClient().GetStringAsync(sb.ToString());
+        }
 
-		public static OnAirStreamsResponse ParseOnAirStreamsData( string onAirStreamsData )
-		{
-			return JsonSerializerExtensions.Load<OnAirStreamsResponse>( onAirStreamsData );
-		}
+        public static OnAirStreamsResponse ParseOnAirStreamsData(string onAirStreamsData)
+        {
+            return JsonSerializerExtensions.Load<OnAirStreamsResponse>(onAirStreamsData);
+        }
 
-		public static Task<OnAirStreamsResponse> GetOnAirStreamsIndexAsync( NiconicoContext context, ushort pageIndex = 1 )
-		{
-			return GetOnAirStreamsIndexDataAsync( context, pageIndex )
-				.ContinueWith( prevTask => ParseOnAirStreamsData( prevTask.Result ) );
-		}
+        public static Task<OnAirStreamsResponse> GetOnAirStreamsIndexAsync(NiconicoContext context, ushort pageIndex = 1)
+        {
+            return GetOnAirStreamsIndexDataAsync(context, pageIndex)
+                .ContinueWith(prevTask => ParseOnAirStreamsData(prevTask.Result));
+        }
 
-		public static Task<OnAirStreamsResponse> GetOnAirStreamsRecentAsync(
-			NiconicoContext context, ushort pageIndex, Category category, SortDirection direction = SortDirection.Ascending, SortType type = SortType.StartTime )
-		{
-			return GetOnAirStreamsRecentDataAsync( context, pageIndex, category, direction, type )
-				.ContinueWith( prevTask => ParseOnAirStreamsData( prevTask.Result ) );
-		}
-	}
+        public static Task<OnAirStreamsResponse> GetOnAirStreamsRecentAsync(
+            NiconicoContext context, ushort pageIndex, Category category, SortDirection direction = SortDirection.Ascending,
+            SortType type = SortType.StartTime)
+        {
+            return GetOnAirStreamsRecentDataAsync(context, pageIndex, category, direction, type)
+                .ContinueWith(prevTask => ParseOnAirStreamsData(prevTask.Result));
+        }
+    }
 }
--- Mntone.Nico2/Mntone.Nico2.Shared/Live/OnAirStreams/OnAirStreamsResponse.cs
+++ Mntone.Nico2/Mntone.Nico2.Shared/Live/OnAirStreams/OnAirStreamsResponse.cs
@@ -3,39 +3,43 @@ using System.Runtime.Serialization;
 
 namespace Mntone.Nico2.Live.OnAirStreams
 {
-	/// <summary>
-	/// 放送中の番組を格納するクラス
-	/// </summary>
-	[DataContract]
-	public sealed class OnAirStreamsResponse
-	{
-		internal OnAirStreamsResponse()
-		{ }
+    /// <summary>
+    ///     放送中の番組を格納するクラス
+    /// </summary>
+    [DataContract]
+    public sealed class OnAirStreamsResponse
+    {
+        private List<OnAirStream> _OnAirStreams;
+        private List<ReservedStream> _ReservedStreams;
 
-		/// <summary>
-		/// 放送中の番組の一覧
-		/// </summary>
-		public IReadOnlyList<OnAirStream> OnAirStreams { get { return this._OnAirStreams; } }
-		private List<OnAirStream> _OnAirStreams = null;
+        /// <summary>
+        ///     放送中の番組の一覧
+        /// </summary>
+        public IReadOnlyList<OnAirStream> OnAirStreams
+        {
+            get { return _OnAirStreams; }
+        }
 
-		[DataMember( Name = "onair_stream_list" )]
-		private List<OnAirStream> OnAirStreamsImpl
-		{
-			get { return this._OnAirStreams ?? ( this._OnAirStreams = new List<OnAirStream>() ); }
-			set { this._OnAirStreams = value; }
-		}
+        [DataMember(Name = "onair_stream_list")]
+        private List<OnAirStream> OnAirStreamsImpl
+        {
+            get { return _OnAirStreams ?? (_OnAirStreams = new List<OnAirStream>()); }
+            set { _OnAirStreams = value; }
+        }
 
-		/// <summary>
-		/// 予約している放送中の番組一覧 (index のみ)
-		/// </summary>
-		public IReadOnlyList<ReservedStream> ReservedStreams { get { return this._ReservedStreams; } }
-		private List<ReservedStream> _ReservedStreams = null;
+        /// <summary>
+        ///     予約している放送中の番組一覧 (index のみ)
+        /// </summary>
+        public IReadOnlyList<ReservedStream> ReservedStreams
+        {
+            get { return _ReservedStreams; }
+        }
 
-		[DataMember( Name = "reserved_stream_list" )]
-		private List<ReservedStream> ReservedStreamsImpl
-		{
-			get { return this._ReservedStreams ?? ( this._ReservedStreams = new List<ReservedStream>() ); }
-			set { this._ReservedStreams = value; }
-		}
-	}
+        [DataMember(Name = "reserved_stream_list")]
+        private List<ReservedStream> ReservedStreamsImpl
+        {
+            get { return _ReservedStreams ?? (_ReservedStreams = new List<ReservedStream>()); }
+            set { _ReservedStreams = value; }
+        }
+    }
 }
